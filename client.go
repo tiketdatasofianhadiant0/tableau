838b90ff712b5d08c0491dbac73d7ce690257f90
Risal <risal.risal@tiket.com>
2022-08-04 10:31:18 +0700

    Add NewClient function

--- client.go
+++ client.go
@@ -1,11 +1,33 @@
 package tableau
 
-import "github.com/go-resty/resty"
+import (
+	"github.com/go-resty/resty/v2"
+	jsoniter "github.com/json-iterator/go"
+)
 
 type Client struct {
 	c              *resty.Client
 	cfg            *Config
 	Authentication *authentication
-	host           string
-	version        string
+}
+
+func NewClient(cfg Config) (*Client, error) {
+	if err := cfg.initConfig(); err != nil {
+		return nil, err
+	}
+
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	restClient := resty.New()
+	restClient.JSONMarshal = json.Marshal
+	restClient.JSONUnmarshal = json.Unmarshal
+
+	client := &Client{
+		c:   restClient,
+		cfg: &cfg,
+	}
+
+	auth := &authentication{base: client}
+	client.Authentication = auth
+
+	return client, nil
 }
