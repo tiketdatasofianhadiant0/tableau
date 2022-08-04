975a83cd08c3159ba31f75d25a504b6ec515e683
Risal <risal.risal@tiket.com>
2022-08-05 00:26:02 +0700

    Add errors

--- errors.go
+++ errors.go
@@ -8,21 +8,29 @@ var (
 	ErrUnknownError                = errors.New("unknown error")
 	ErrFailedUnmarshalResponseBody = errors.New("failed to unmarshal response body")
 
-	ErrForbidden             = errors.New("user do not have sufficient permissions")
-	ErrServerNotFound        = errors.New("server was not found")
-	ErrInvalidRequestMethod  = errors.New("not a valid request type")
-	ErrBadRequest            = errors.New("the content of the request body is missing or incomplete")
-	ErrLoginError            = errors.New("the credentials are invalid or blocked")
-	ErrPayloadToLarge        = errors.New("request body is too large to process")
-	ErrTooManyRequest        = errors.New("request limit reached")
-	ErrInternalServerError   = errors.New("tableau server error")
-	ErrInternalServiceError  = errors.New("tableau service error")
-	ErrBroadcastServiceError = errors.New("broadcast service error")
+	ErrForbidden              = errors.New("user do not have sufficient permissions")
+	ErrServerNotFound         = errors.New("server was not found")
+	ErrInvalidRequestMethod   = errors.New("not a valid request type")
+	ErrBadRequest             = errors.New("the content of the request body is missing or incomplete")
+	ErrLoginError             = errors.New("the credentials are invalid (wrong username/password) or blocked")
+	ErrPayloadToLarge         = errors.New("request body is too large to process")
+	ErrTooManyRequest         = errors.New("request limit reached")
+	ErrNoCredential           = errors.New("no credentials were provided")
+	ErrInvalidCredential      = errors.New("invalid credentials were provided")
+	ErrSwitchSiteError        = errors.New("cannot switch site; the site might be unavailable or was not found")
+	ErrCannotSwitchToSameSite = errors.New("cannot switch to the same site")
+	ErrInternalServerError    = errors.New("tableau server error")
+	ErrInternalServiceError   = errors.New("tableau service error")
+	ErrBroadcastServiceError  = errors.New("broadcast service error")
 
 	errCodeMap = map[string]error{
 		"400000": ErrBadRequest,
+		"401000": ErrNoCredential,
 		"401001": ErrLoginError,
+		"401002": ErrInvalidCredential,
+		"401003": ErrSwitchSiteError,
 		"403004": ErrForbidden,
+		"403070": ErrCannotSwitchToSameSite,
 		"404000": ErrServerNotFound,
 		"405000": ErrInvalidRequestMethod,
 		"413000": ErrPayloadToLarge,
