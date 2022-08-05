2ea2f6d99899f17c3b5d411bdbe651d1ffaacd4a
Risal <risal.risal@tiket.com>
2022-08-05 09:53:23 +0700

    Add group and import to model

--- models/vars.go
+++ models/vars.go
@@ -2,6 +2,16 @@ package models
 
 import jsoniter "github.com/json-iterator/go"
 
+const (
+	SiteRoleUnlicensed                = `Unlicensed`
+	SiteRoleViewer                    = `Viewer`
+	SiteRoleExplorer                  = `Explorer`
+	SiteRoleExplorerCanPublish        = `ExplorerCanPublish`
+	SiteRoleCreator                   = `Creator`
+	SiteRoleSiteAdministratorExplorer = `SiteAdministratorExplorer`
+	SiteRoleSiteAdministratorCreator  = `SiteAdministratorCreator`
+)
+
 var (
 	json = jsoniter.ConfigCompatibleWithStandardLibrary
 )
